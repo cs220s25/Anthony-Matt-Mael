7a1457d8eb712136005d933642ef0db7aece45c0
Mael Tshiyonga <tshiyongam@moravian.edu>
2025-04-15 10:52:59 -0400

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 yum install git -y
-yum install maven -y
 yum install redis6 -y
 yum install docker -y
-cd /home/ec2-user
+yum install -y maven-amazon-corretto21
 git clone https://github.com/cs220s25/Anthony-Matt-Mael.git 
 cd Anthony-Matt-Mael
 systemctl enable redis6
-systemctl restart redis6
 systemctl start redis6
 mvn clean package
-java -jar target/dbot-1.0-SNAPSHOT.jar 
+java -jar target/dbot-1.0-SNAPSHOT.jar
