575ffa6406f5b34962677995b499fa79cdd22a16
Mael <tshiyongam@moravian.edu>
2025-04-26 23:13:18 -0400

    Create pushhooks.sh script and edit workflow

---
+++ pushhooks.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Running Checkstyle before pushing..."
+mvn --batch-mode checkstyle:check
+if [ $? -ne 0 ]; then
+  echo "Checkstyle violations found. Push canceled."
+  exit 1
+fi
+echo "Checkstyle passed. Push allowed."
+exit 0
